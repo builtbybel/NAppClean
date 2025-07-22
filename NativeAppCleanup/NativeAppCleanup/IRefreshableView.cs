10a00cf3b0311fca88b2743b8dad6a0d483b4f3c
Belim <belim@builtbybel.com>
2025-07-22 18:13:21 +0200

    Init NAppClean, Upt-IRefreshableView

--- NativeAppCleanup/NativeAppCleanup/IRefreshableView.cs
+++ NativeAppCleanup/NativeAppCleanup/IRefreshableView.cs
@@ -4,8 +4,6 @@
     /// <summary>
     /// Represents a view that can be refreshed to update its display or data.
     /// </summary>
-    /// <remarks>Implement this interface in views that require a mechanism to refresh their content, such as
-    /// updating displayed data or re-rendering UI elements.</remarks>
     public interface IRefreshableView
     {
         void RefreshView();
